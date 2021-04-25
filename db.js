744a909a9ba9e951f454c209d772fe43eef14667
jihye kang <jihye59kang@gmail.com>
2021-04-25 23:07:48 +0100

    add db

---
+++ db.js
@@ -0,0 +1,38 @@
+export const videos = [
+    {
+        id:324393,
+        title: 'Video awesome',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 1111,
+            name: "bonnie",
+            email: "jh@kang.com"
+        }
+    },
+    {
+        id:324395,
+        title: 'Video nice',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 1111,
+            name: "bonnie",
+            email: "jh@kang.com"
+        }
+    },
+    {
+        id:324394,
+        title: 'Video super',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 1111,
+            name: "bonnie",
+            email: "jh@kang.com"
+        }
+    }
+]
