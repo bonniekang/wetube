5d6360bbfdd3ee41bace215903b1f3ec280ade8f
jihye kang <jihye59kang@gmail.com>
2021-06-03 19:46:50 +0100

    login page

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/User"
+import bcrypt from "bcrypt"
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join"});
 export const postJoin = async (req, res) => {
@@ -11,17 +12,45 @@ export const postJoin = async (req, res) => {
     if(exists){
         return res.status(400).render('join', { pageTitle, errorMessage: "This username/email is already taken." })
     }
-
-    await User.create({
-        name,
-        username,
-        email,
-        password,
-        location,
-    })
-    return res.redirect("/login");
+    try {
+        await User.create({
+            name,
+            username,
+            email,
+            password,
+            location,
+        })
+        return res.redirect("/login");
+    }catch(error){
+        return res.status(400).render(
+            "join", {
+                pageTitle,
+                errorMessage: error._message,
+            })
+    }
 }
-export const login = (req, res) => res.render("login", { pageTitle: "Login"});
+
+export const getLogin = (req, res) => res.render("login", { pageTitle: "Login"});
+
+export const postLogin = async (req, res) => {
+    const { username, password } = res.body;
+    // check if account exists
+    const pageTitle = "Login"
+    const user = await User.findOne({username});
+    if(!user) {
+        return res.status(400).render("login", { pageTitle, errorMessage: "This username does not exist."})
+    }
+
+    // check if password correct
+    const match = await bcrypt.compare(password, user.password)
+    if(!match){
+        return res.status(400).render("login", { pageTitle , errorMessage: "Wrong password"})
+    }
+
+    return res.redirect("/");
+}
+
+
 export const logout = (req, res) => res.render("logout", { pageTitle: "Logout"});
 export const userDetail = (req, res) => res.render('userDetail', { pageTitle: "User Detail"});
 export const editProfile = (req, res) => res.render('editProfile', { pageTitle: "Edit Profile"});
