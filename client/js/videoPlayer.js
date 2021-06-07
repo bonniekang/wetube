33c705510e0b1a6fbf4e543b0cec66e5a278b80d
jihye kang <jihye59kang@gmail.com>
2021-06-07 16:51:16 +0100

    video play pause event

---
+++ client/js/videoPlayer.js
@@ -0,0 +1,25 @@
+const video = document.querySelector("video")
+const playBtn = document.getElementById("play")
+const muteBtn = document.getElementById("mute")
+const time = document.getElementById("time")
+const volume = document.getElementById("volume")
+
+const handlePlayClick = (e) => {
+    if(video.paused){
+        video.play()
+    }else{
+        video.pause()
+    }
+}
+
+const handleMute = (e) => {
+
+}
+
+const handlePlay = () => playBtn.innerText = "Pause"
+const handlePause = () => playBtn.innerText = "Play"
+
+playBtn.addEventListener("click", handlePlayClick)
+muteBtn.addEventListener("click", handleMute)
+video.addEventListener("pause", handlePause)
+video.addEventListener("play", handlePlay)
